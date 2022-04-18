6d96441e6076ae8c9f301347bedf4bfb9dce59c5
LeonardoBizzoni <leo2002714@gmail.com>
2022-04-18 20:19:17 +0200

    Improved live vtuber page

--- www/views/live.php
+++ www/views/live.php
@@ -16,18 +16,19 @@ if (!isset($_GET["id"])) {
     foreach ($params[0] as $vtuber) {
         if ($_GET["id"] == $vtuber["id"]) {
             echo "
-<div id=\"parent\">
+    <div id=\"parent\">
     <div>
-      <nav class=\"navbar bg-dark text-white\">
-          <div class=\"container-fluid\">
-                  <h1>" . ucfirst($vtuber["username"]) . "</h1><img src='".$vtuber["img"]."'/>
-          </div>
-      </nav>
-    </div>
+    <nav class=\"navbar bg-dark text-white\">
+        <div class=\"container-fluid\">
+                <h1>" . ucfirst($vtuber["username"]) . "</h1><img class=\"idolLogo\" src=\"" . $vtuber["img"] . "\"/>
+        </div>
+    </nav>
+
 </div>
 
 <div id=\"child\">
-<iframe src=\"https://player.twitch.tv/?channel=" . $vtuber["login"] . "&parent=localhost\" frameborder=\"0\" allowfullscreen=\"true\" scrolling=\"no\"></iframe>
+<iframe src=\"https://player.twitch.tv/?channel=".$vtuber["login"]."&parent=localhost\" frameborder=\"0\" allowfullscreen=\"true\" scrolling=\"no\"></iframe>
+</div>
 </div>
 ";
         }
